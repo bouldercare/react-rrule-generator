eda112e2483f82c38304f8b87b0317306f7d2c24
Dave Lerman <david.lerman@1deg.org>
2018-09-19 16:33:35 -0700

    gitpkg

---
+++ src/lib/components/End/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import EndAfter from './After';
+import EndOnDate from './OnDate';
+
+const End = ({
+  end: {
+    mode,
+    after,
+    onDate,
+    options,
+  },
+  handleChange,
+}) => {
+  const isOptionAvailable = option => !options.modes || options.modes.indexOf(option) !== -1;
+  const isOptionSelected = option => mode === option;
+
+  return (
+    <div className="px-3">
+      <div className="form-group row">
+        <div className="col-sm-2 text-sm-right">
+          <label
+            htmlFor="End"
+            className="col-form-label"
+          >
+            <strong>
+              End
+            </strong>
+          </label>
+        </div>
+        <div className="col-sm-3">
+          <select
+            name="end.mode"
+            id="End"
+            className="form-control"
+            value={mode}
+            onChange={handleChange}
+          >
+            {isOptionAvailable('Never') && <option value="Never">Never</option>}
+            {isOptionAvailable('After') && <option value="After">After</option>}
+            {isOptionAvailable('On date') && <option value="On date">On date</option>}
+          </select>
+        </div>
+
+        {isOptionSelected('After') && <EndAfter after={after} handleChange={handleChange} />}
+        {isOptionSelected('On date') && <EndOnDate onDate={onDate} handleChange={handleChange} />}
+
+      </div>
+    </div>
+  );
+};
+
+End.propTypes = {
+  end: PropTypes.shape({
+    mode: PropTypes.string.isRequired,
+    after: PropTypes.number.isRequired,
+    onDate: PropTypes.object.isRequired,
+    options: PropTypes.shape({
+      modes: PropTypes.arrayOf(PropTypes.oneOf(['Never', 'After', 'On date'])),
+      weekStartsOnSunday: PropTypes.bool,
+    }).isRequired,
+  }).isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default End;
---
+++ src/lib/components/ReactRRuleGenerator.js
@@ -0,0 +1,130 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { cloneDeep, set } from 'lodash';
+
+import Start from './Start/index';
+import Repeat from './Repeat/index';
+import End from './End/index';
+import computeRRuleToString from '../utils/computeRRule/toString/computeRRule';
+import computeRRuleFromString from '../utils/computeRRule/fromString/computeRRule';
+import configureInitialState from '../utils/configureInitialState';
+import '../styles/index.css';
+
+class ReactRRuleGenerator extends PureComponent {
+  // compute default view based on user's config
+  state = configureInitialState(this.props.config, this.props.calendarComponent);
+
+  componentWillMount() {
+    if (this.props.onChange === ReactRRuleGenerator.defaultProps.onChange) {
+      // no onChange() was provided
+      throw new Error('No onChange() function has been passed to RRuleGenerator. \n' +
+        'Please provide one, it\'s needed to handle generated value.');
+    }
+
+    if (this.props.value) {
+      // if value is provided to RRuleGenerator, it's used to compute state of component's forms
+      const data = computeRRuleFromString(this.state.data, this.props.value);
+      this.setState({ data, rrule: this.props.value });
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.value) {
+      const data = computeRRuleFromString(this.state.data, nextProps.value);
+      this.setState({ data, rrule: nextProps.value });
+    }
+  }
+
+  handleChange = ({ target }) => {
+    const newData = cloneDeep(this.state.data);
+    set(newData, target.name, target.value);
+    const rrule = computeRRuleToString(newData);
+
+    this.setState({ data: newData, rrule });
+    this.props.onChange(rrule);
+  };
+
+  render() {
+    const {
+      data: {
+        start,
+        repeat,
+        end,
+        options,
+        error,
+      },
+    } = this.state;
+
+    return (
+      <div>
+
+        {
+          !options.hideError && error && (
+            <div className="alert alert-danger">
+              You provided an invalid RRule value to component. {`'${error.value}'`} is not a correct RRule string.
+            </div>
+          )
+        }
+
+        <div className="px-0 pt-3 border rounded">
+
+          {
+            !options.hideStart && (
+              <div>
+                <Start
+                  start={start}
+                  handleChange={this.handleChange}
+                />
+                <hr />
+              </div>
+            )
+          }
+
+          <div>
+            <Repeat
+              repeat={repeat}
+              handleChange={this.handleChange}
+            />
+          </div>
+
+          {
+            !options.hideEnd && (
+              <div>
+                <hr />
+                <End
+                  end={end}
+                  handleChange={this.handleChange}
+                />
+              </div>
+            )
+          }
+
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactRRuleGenerator.propTypes = {
+  config: PropTypes.shape({
+    frequency: PropTypes.arrayOf(PropTypes.oneOf(['Yearly', 'Monthly', 'Weekly', 'Daily', 'Hourly'])),
+    yearly: PropTypes.oneOf(['on', 'on the']),
+    monthly: PropTypes.oneOf(['on', 'on the']),
+    end: PropTypes.arrayOf(PropTypes.oneOf(['Never', 'After', 'On date'])),
+    hideStart: PropTypes.bool,
+    hideEnd: PropTypes.bool,
+    hideError: PropTypes.bool,
+    weekStartsOnSunday: PropTypes.bool,
+  }),
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  calendarComponent: PropTypes.oneOfType([PropTypes.element, PropTypes.func]),
+};
+ReactRRuleGenerator.defaultProps = {
+  value: '',
+  config: {},
+  onChange() {},
+  calendarComponent: null,
+};
+
+export default ReactRRuleGenerator;
